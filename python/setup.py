942ef77883a0664ce0360e1ebb47ea0eb8bbdd91
Ryan Munro <ryan@submersible.io>
2018-05-15 11:24:15 -0700

    Add setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,2 +1,8 @@
-# Your python setup file. An example can be found at:
-# https://github.com/pypa/sampleproject/blob/master/setup.py
+from setuptools import setup
+
+setup(
+    name='graphframes',
+    version='0.5.0',
+    packages=['graphframes', 'graphframes.lib']
+)
+
